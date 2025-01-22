684a887d4e6fd514332b53e8d7b3a4d0b5ab689d
jajularamya <ramyajajula111@gmail.com>
2025-01-22 07:52:47 +0530

    shell script

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,10 +2,10 @@
 
 NUMBER=$1
 
-if [$NUMBER -gt 100 ]
+if [$NUMBER -gt 1000 ]
  then
-  echo "given number is greater than 100"
+  echo "given number is greater than 1000"
   else
-  echo "given number is less than or equal to 100"
+  echo "given number is less than or equal to 1000"
   
   fi
