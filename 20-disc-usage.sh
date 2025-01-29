c60087a62736a08284111d16159e3ee9d0ff9840
jajularamya <ramyajajula111@gmail.com>
2025-01-30 03:27:56 +0530

    shell-script

--- 20-disc-usage.sh
+++ 20-disc-usage.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 DISC_USAGE=$(df -hT | grep xfs)
-
+DISC_THRESHOLD=5
 while read -r line
 do
-  echo $line
+  USAGE=$(echo $line | awk -F " " '{print $6F}')
+  PARTITION=$(echo $line | awk -F " " '{print $NF}')
+  echo "parition: $PARTITION , usage: $USAGE"
 done <<< $DISC_USAGE
