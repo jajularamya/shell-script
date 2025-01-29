3e35893cea30673a69bea377adfa925a58c3e906
jajularamya <ramyajajula111@gmail.com>
2025-01-30 04:09:33 +0530

    shell-script

--- 21-script-1.sh
+++ 21-script-1.sh
@@ -5,5 +5,5 @@ GREET="hello from SCRIPT-1"
 source ./22-script-2.sh
 
 echo "hello from: $MSG"
-
+echo "A value: $A"
 #sh 22-script-2.sh
--- 22-script-2.sh
+++ 22-script-2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 MSG="SCRIPT2"
-
+A="10"
 echo "hello from: $MSG"
 echo "greeting: $GREET" 
