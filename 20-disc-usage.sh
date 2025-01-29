245ec8c049d7ca5c3c3cb81e55e7c04dd11cc62c
jajularamya <ramyajajula111@gmail.com>
2025-01-30 03:33:56 +0530

    shell-script

--- 20-disc-usage.sh
+++ 20-disc-usage.sh
@@ -6,5 +6,11 @@ while read -r line
 do
   USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
   PARTITION=$(echo $line | awk -F " " '{print $NF}')
-  echo "parition: $PARTITION , usage: $USAGE"
+  #echo "parition: $PARTITION , usage: $USAGE"
+  if[ $USAGE -ge $DISC_THRESHOLD ]
+  then
+   MSG="high disc message on partition: $PARTITION usage is: $USAGE"
+  fi 
 done <<< $DISC_USAGE
+
+echo "message: $MSG"
